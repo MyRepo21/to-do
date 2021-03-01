2ddb70621f2c399bd66095b524464e11da478622
MyRepo21 <student2020@nym.hush.com>
2021-03-01 14:42:02 -0800

    Updates

--- to-do.js
+++ to-do.js
@@ -1,12 +1,13 @@
-/*
 
-*/
+
 
 function getTask(e) {
-   e.preventDefault();
-   var task = document.getElementById("taskInput");
-   alert(task.value);
-}
+  e.preventDefault();
+  var newTask = document.getElementById("taskInput");
+  var taskList = document.createElement("taskList");
+  var firstTaskNode = document.createTextNode(newTask.value);
+  taskList.appendChild(newTask.value)
+  }
 
 const submitBtn = document.querySelector("#addButton");
 submitBtn.addEventListener("click", getTask);
