518db01395b60942fe6ef96f9fb8cf64d4ce082e
MyRepo21 <student2020@nym.hush.com>
2021-03-01 15:02:04 -0800

    Updates errors

--- to-do.js
+++ to-do.js
@@ -3,11 +3,12 @@
 
 function getTask(e) {
   e.preventDefault();
+  
   var newTask = document.getElementById("taskInput");
-  var taskList = document.createElement("taskList");
+  var taskList = document.createElement('ul');
   var firstTaskNode = document.createTextNode(newTask.value);
   taskList.appendChild(newTask.value)
   }
 
-const submitBtn = document.querySelector("#addButton");
-submitBtn.addEventListener("click", getTask);
+  const submitBtn = document.querySelector("#addButton");
+  submitBtn.addEventListener("click", getTask);
