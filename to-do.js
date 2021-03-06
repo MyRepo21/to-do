07a70c4caa59d31cec9a1cf9aaf2e50e09e9c2fc
MyRepo21 <student2020@nym.hush.com>
2021-03-06 15:39:16 -0800

    Edits

--- to-do.js
+++ to-do.js
@@ -1,40 +1,17 @@
-/*
-create task input field 
-create addButton
-? create an unordered list as a placeholder in HTML?  or create in JS only when button clicked?
-get task (on button click) getElementByID
-  create list element for task li  createElement 
-    add task to task list appendChild
-      with each task include inline doneButton and deleteButton
-        on doneButton click, change task style to strikethrough
-        on deleteButton click, remove task from list
-  clear task input field
-
-mark task complete
-*/
-
-/* 
 function getTask(e) {
-    e.preventDefault();
-    addButton.addEventListener("click", getTaskSubFcn()) {
-      getTaskSubFcn {}
-    };
-    */
-
-
-function getTask(e) {
-    e.preventDefault();
-         if (task = " ") {
-         alert("Please enter a task.");
-       }
-
-    let taskList = document.createElement("li");
-    let taskListItem = taskList.appendChild(task).innerHTML;
-    document.getElementById("task").value = "";
-   }
+     e.preventDefault();
+   var task = document.getElementById("taskInput");
+   /*if (task = " ") {
+   alert("Please enter a task.");
+  }*/
+  var taskList = document.getElementById("taskList");
+  
+  taskList.append(task.value);
+  task.value = "";
+}
 
   function markTaskDone() {
-       let task.style.textDecoration = "line-through";
+  task.style.textDecoration = "line-through";
 
   }
 
@@ -42,23 +19,12 @@ function getTask(e) {
     taskList.removeChild("task");
   }
 
-  let task = document.getElementById("task").value; 
   const taskList = document.createElement("taskList");
-  const addButton = document.getElementByID("addButton");
-  const doneButton = document.getElementByID("doneButton");
-  const deleteButton = document.getElementByID("deleteButton");
+  const doneButton = document.getElementById("doneButton");
+  const deleteButton = document.getElementById("deleteButton");
+  const addButton = document.querySelector("#addButton");
+
 
   addButton.addEventListener("click", getTask);
-  doneButton.addEventListener("click", markTaskDone);
-  deleteButton.addEventListener("click", deleteTask);
-
-  
-  
-
-
-  
-
-
-
-    
-
+  //doneButton.addEventListener("click", markTaskDone);
+ // deleteButton.addEventListener("click", deleteTask);
