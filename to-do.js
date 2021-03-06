590c75b8bfdc4412d4e8f8fc1f3215b254fa75da
MyRepo21 <student2020@nym.hush.com>
2021-03-06 10:33:50 -0800

    Edits

--- to-do.js
+++ to-do.js
@@ -1,28 +1,61 @@
 /*
-create task input field, button HTML
+create task input field 
+create addButton
 ? create an unordered list as a placeholder in HTML?  or create in JS only when button clicked?
-get task (on button click)
-  create list element for task li   
-    add task to task list (append)
+get task (on button click) getElementByID
+  create list element for task li  createElement 
+    add task to task list appendChild
+      with each task include inline doneButton and deleteButton
+        on doneButton click, change task style to strikethrough
+        on deleteButton click, remove task from list
   clear task input field
-delete task 
 
 mark task complete
 */
 
-const taskList = document.getElementByID("taskList");
+
 const addButton = document.querySelector("#addButton");
+
 
 function getTask(e) {
 
     e.preventDefault();
 
-    var task = document.getElementById("task").value;
+    var task = document.getElementById("task").value; //get task from user
+    var taskList = document.createElement("li");
     taskList.appendChild(task);
-  
+    <input type="image" src="ito-do-app/images/imgDeleteButton.PNG" alt="Submit" width="416" height="16"> 
   }
-  const taskInput = document.getElementById("taskInput");
+
+  function createDoneButton() {  
+    const doneButton = document.createElement("button");
+    doneButton.innerHTML="Done";
+    document.taskList.appendChild(doneButton);  
+  }
+
+  function createDeleteButton() {  
+    const deleteButton = document.createElement("button");
+    deleteButton.innerHTML="Delete";
+    document.taskList.appendChild(deleteButton);
+  }
+  
+  function markTaskDone() {
+    const doneButton = document.getElementById("doneButton");
+    const taskList = document.createElement("taskList");//style list element task with strikethrough
+  }
+
+  function deleteTask() {
+    const deleteButton = document.getElementById("deleteButton");
+    deleteButton.addEventListener("click", deleteTask);
+    taskList.removeChild("task");
+  }
+ 
   addButton.addEventListener("click", getTask);
+  createDoneButton();
+  createDeleteButton();
+  doneButton.addEventListener("click", markTaskDone);
+
+  
   
 
 
