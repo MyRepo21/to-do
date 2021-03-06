8f1185d55e59698e64ea6a6344a1ca772646bab4
MyRepo21 <student2020@nym.hush.com>
2021-03-06 06:41:45 -0800

    Edits

--- to-do.js
+++ to-do.js
@@ -1,11 +1,34 @@
-function getTask(e) {
-  e.preventDefault();
-  
-  var newTask = document.getElementById("taskInput");
-  var taskList = document.createElement('ul');
-  //var firstTaskNode = document.createTextNode(newTask.value);
-  taskList.append(newTask.value)
-  }
+/*
+create task input field, button HTML
+? create an unordered list as a placeholder in HTML?  or create in JS only when button clicked?
+get task (on button click)
+  create list element for task li   
+    add task to task list (append)
+  clear task input field
+delete task 
 
-  const submitBtn = document.querySelector("#addButton");
-  submitBtn.addEventListener("click", getTask);
+mark task complete
+*/
+
+const taskList = document.getElementByID("taskList");
+const addButton = document.querySelector("#addButton");
+
+function getTask(e) {
+
+    e.preventDefault();
+
+    var task = document.getElementById("task").value;
+    taskList.appendChild(task);
+  
+  }
+  const taskInput = document.getElementById("taskInput");
+  addButton.addEventListener("click", getTask);
+  
+
+
+  
+
+
+
+    
+
