dd7038ee12bd1e93a978a2f503fc977eb6f7052c
MyRepo21 <78832519+MyRepo21@users.noreply.github.com>
2021-03-07 11:18:39 -0800

    Update to-do.js

--- to-do.js
+++ to-do.js
@@ -12,3 +12,16 @@
     taskList.append(taskListItem);
     taskInputString.value = "";
     })
+
+var taskListItem = document.getElementById("taskList");
+var taskList = document.getElementById("taskList");
+taskListItem.addEventListener("click", () => {
+var prompt = window.prompt("1 = Done, 2 = Delete"); 
+  if (prompt == 1) {
+    taskListItem.style.textDecoration = "line-through";
+  } else
+  if (prompt == 2) {
+    taskListItem.removeChild(taskList); //Uncaught DOMException: Node.removeChild: The node to be removed is not a child of this node
+  }
+});
+  //alert("abc");
