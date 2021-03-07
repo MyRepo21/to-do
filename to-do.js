f5c204cbcd14883abbcb6919bffd07f2651d4f18
MyRepo21 <student2020@nym.hush.com>
2021-03-07 10:04:16 -0800

    Edits

--- to-do.js
+++ to-do.js
@@ -1,8 +1,7 @@
   const addButton = document.querySelector("#addButton");
-  const doneButton = document.getElementById("doneButton");
-  const deleteButton = document.getElementById("deleteButton");
-  //var taskInput = document.getElementById("taskInput");
-   
+  const OKButton = document.getElementById("OKButton");
+  const cancelButton = document.getElementById("cancelButton");
+
     addButton.addEventListener("click", () => {
     var taskInput = document.getElementById("taskInput");
     var taskInputString = taskInput.value;
@@ -12,20 +11,4 @@
     taskListItem.append(taskInputString);
     taskList.append(taskListItem);
     taskInputString.value = "";
-  });
-  
-  //taskList.addEventListener("click", () => {
-  //  alert("Mark Done or Delete?");
- // });
-  //user clicks on list item, pop-up alert with done button and delete button, when click
-  //doneOrDeleteButton.addEventListener("click", () => {
-    // if user left-clicks to select specific list item element and presses the "c" key, cross out item
-    
-    //taskListItem.style.textDecoration = "line-through";
-  //});
-
- // deleteButton.addEventListener("click", () => {
-   //if user selects specific list item element and presses the "delete" key, deletes the item
-    //taskList.removeChild("task");
- // });
- 
+    })
